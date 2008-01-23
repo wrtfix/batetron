71c658c1c3a99b1ff202d3e832195fad8310849b
wrtfix@gmail.com <wrtfix@gmail.com@8f21a9b6-0343-0410-8382-9dfc9e905893>
2008-01-23 02:01:45 +0000

    Falta utilizar los objetos en el main.cpp

---
+++ trunk/ batetron/main.cpp
@@ -0,0 +1,20 @@
+#include <cstdlib>
+#include <iostream>
+#include <SDL/SDL.h>
+#include <SDL/SDL_mixer.h>
+#include "cuerpo.h"
+#include "bateria.h"
+
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+    Bateria a;
+
+    Cuerpo c;
+    
+    
+    system("PAUSE");
+    return EXIT_SUCCESS;
+}

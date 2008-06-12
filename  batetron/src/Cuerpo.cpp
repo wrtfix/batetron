c493eb72b678471984b7f6c6c6a79e55b8943213
wrtfix <wrtfix@8f21a9b6-0343-0410-8382-9dfc9e905893>
2008-06-12 08:45:37 +0000

    Nuegos cambion, mejor organizacion

---
+++  batetron/src/Cuerpo.cpp
@@ -0,0 +1,34 @@
+
+#include "Cuerpo.h" // class's header file
+
+// class constructor
+Cuerpo::Cuerpo(char *nombre,char *direccion, int volumen, int canal)
+{
+    this->nombre = nombre;
+    this->sonido = Mix_LoadWAV(direccion);
+    this->volumen = volumen;
+    this->canal = canal;
+}
+
+void Cuerpo::setVolumen(int volumen)
+{
+    this->volumen = volumen;
+    Mix_VolumeChunk(sonido, volumen);
+}
+
+int Cuerpo::getVolumen()
+{
+    return this->volumen;
+}
+
+void Cuerpo::Reproducir()
+{
+	Mix_PlayChannel(this->canal, sonido,0);
+	printf("%s",this->nombre);
+	printf("\n");
+}
+// class destructor
+Cuerpo::~Cuerpo()
+{
+    Mix_FreeChunk(sonido);
+}
